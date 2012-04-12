be1d1424cce0b37c23bfb9868f6b74f8e8f6b64d
Paul <tacid@tacid.kiev.ua>
2012-04-12 20:44:52 +0300

    Added check for defined Rails::Railtie

--- lib/liqpay/railtie.rb
+++ lib/liqpay/railtie.rb
@@ -1,9 +1,11 @@
 require 'liqpay/liqpay_helper'
 
 module Liqpay
+if defined?(::Rails::Railtie)
   class Railtie < Rails::Railtie
     initializer 'liqpay.view_helpers' do |app|
       ActionView::Base.send :include, Liqpay::LiqpayHelper 
     end
   end
+end
 end
